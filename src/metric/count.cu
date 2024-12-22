ceb4f124cd14b4d003d14dadb2b223a0e16729b9
Darisishe <konev.sa@phystech.edu>
2024-12-22 19:40:09 +0300

    CountingMetric now supports CPU run

--- src/metric/count.cu
+++ src/metric/count.cu
@@ -1,11 +1,11 @@
-#include <vector>
 #include <string>
+#include <vector>
 
 #define MAX_VALUE 256
 #define BLOCKS 32
 #define THREADS 128
 
-__global__ void count(int* array, int* output, size_t n) {
+__global__ void count(int *array, int *output, size_t n) {
 
   size_t b_id = blockIdx.x;
   size_t b_num = gridDim.x;
@@ -49,40 +49,54 @@ __global__ void count(int* array, int* output, size_t n) {
     memcpy(&output[offset], &count[t_offset], sizeof(int) * t_size);
 }
 
-std::vector<size_t> count_symbols(const std::string& str) {
-	size_t n = str.size();
-	
-	int* array = new int[n];
-	for (size_t i = 0; i < n; ++i) {
-		array[i] = static_cast<unsigned char>(str[i]);
-	}
+std::vector<size_t> count_symbols(const std::string &str) {
+  size_t n = str.size();
+  int deviceCount = 0;
+  cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
 
-	int* d_array;
-	int* d_output;
-	cudaMalloc((void**)&d_array, sizeof(int) * n);
-	cudaMalloc((void**)&d_output, sizeof(int) * BLOCKS * MAX_VALUE);
+  if (error_id != cudaSuccess || deviceCount == 0) {
+    // If no GPU available, just run on CPU
+    std::vector<size_t> counts(MAX_VALUE);
+    for (auto c : str) {
+      ++counts[static_cast<unsigned char>(c)];
+    }
 
-	cudaMemcpy(d_array, array, sizeof(int) * n, cudaMemcpyHostToDevice);
-	cudaMemset(d_output, 0, sizeof(int) * BLOCKS * MAX_VALUE);
+    return counts;
+  
+  } else {
 
-	count<<<BLOCKS, THREADS>>>(d_array, d_output, n);
+    int *array = new int[n];
+    for (size_t i = 0; i < n; ++i) {
+      array[i] = static_cast<unsigned char>(str[i]);
+    }
 
-	int output[BLOCKS * MAX_VALUE];
-	cudaMemcpy(output, d_output, sizeof(int) * BLOCKS * MAX_VALUE, cudaMemcpyDeviceToHost);
+    int *d_array;
+    int *d_output;
+    cudaMalloc((void **)&d_array, sizeof(int) * n);
+    cudaMalloc((void **)&d_output, sizeof(int) * BLOCKS * MAX_VALUE);
 
+    cudaMemcpy(d_array, array, sizeof(int) * n, cudaMemcpyHostToDevice);
+    cudaMemset(d_output, 0, sizeof(int) * BLOCKS * MAX_VALUE);
 
-	// output contains chunks of counts
-	std::vector<size_t> counts(MAX_VALUE);
-	for (size_t i = 0; i < MAX_VALUE; ++i) {
-		for (size_t j = 0; j < BLOCKS; ++j) {
-			counts[i] += output[j * MAX_VALUE + i];
-		}
-	}
+    count<<<BLOCKS, THREADS>>>(d_array, d_output, n);
 
-	cudaFree(d_array);
-	cudaFree(d_output);
+    int output[BLOCKS * MAX_VALUE];
+    cudaMemcpy(output, d_output, sizeof(int) * BLOCKS * MAX_VALUE,
+               cudaMemcpyDeviceToHost);
 
-	delete[] array;
+    // output contains chunks of counts
+    std::vector<size_t> counts(MAX_VALUE);
+    for (size_t i = 0; i < MAX_VALUE; ++i) {
+      for (size_t j = 0; j < BLOCKS; ++j) {
+        counts[i] += output[j * MAX_VALUE + i];
+      }
+    }
 
-	return counts;
+    cudaFree(d_array);
+    cudaFree(d_output);
+
+    delete[] array;
+
+    return counts;
+  }
 }
